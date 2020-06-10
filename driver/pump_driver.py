083a37cbf0d8c370929c314f4397ab91ce5183bc
helgestein <helge.stein@rub.de>
2020-06-10 16:52:05 +0200

    kkkkkkk

--- driver/pump_driver.py
+++ driver/pump_driver.py
@@ -4,122 +4,109 @@
 
 import serial
 import time
-import uvicorn
-from fastapi import FastAPI
 from pydantic import BaseModel
-
-app = FastAPI(title="Pump server V1",
-    description="This is a very fancy pump server",
-    version="1.0",)
 
 class return_class(BaseModel):
     measurement_type: str = None
     parameters: dict = None
     data: dict = None
 
-@app.get("/pump/isBlocked")
-def isBlocked(pump: int):
-    #this is nessesary since there is no serial command that says "pump is still pumping"
-    if conf['pumpBlockings'][pump] >= time.time():
-        retc = return_class(
-                    measurement_type="pump_command",
-                    parameters={"command": "isBlocked","pump":pump},
-                    data={'status':True}
-                )
-        return retc
-    else:
+class pump():
+    def __init__():
+            self.conf = dict(port='COM1', baud=9600, timeout=1,
+                        pumpAddr={i: i + 21 for i in range(14)},  # numbering is left to right top to bottom
+                        pumpBlockings={i: time.time() for i in range(14)},  # init the blockings with now
+                        )
+            #i set the serial connection here once
+            self.ser = serial.Serial(conf['port'], conf['baud'], timeout=conf['timeout'])
+
+    def isBlocked(self, pump: int):
+        #this is nessesary since there is no serial command that says "pump is still pumping"
+        if self.conf['pumpBlockings'][pump] >= time.time():
+            retc = return_class(
+                        measurement_type="pump_command",
+                        parameters={"command": "isBlocked","pump":pump},
+                        data={'status':True}
+                    )
+            return retc
+        else:
+            retc = return_class(
+                measurement_type="pump_command",
+                parameters={"command": "isBlocked"},
+                data={'status': False}
+            )
+            return retc
+
+    def setBlock(self, pump:int, time_block:float):
+        #this sets a block
+        self.conf['pumpBlockings'][pump] = time_block
         retc = return_class(
             measurement_type="pump_command",
-            parameters={"command": "isBlocked"},
-            data={'status': False}
+            parameters={"command": "block","time_block":time_block},
         )
         return retc
 
-@app.get("/pump/setBlock")
-def setBlock(pump:int, time_block:float):
-    #this sets a block
-    conf['pumpBlockings'][pump] = time_block
-    retc = return_class(
-        measurement_type="pump_command",
-        parameters={"command": "block","time_block":time_block},
-    )
-    return retc
 
-@app.get("/pump/dispenseVolume")
-def dispenseVolume(pump:int ,volume:int ,speed:int ,direction:int=1,read=False):
-    #pump is an index 0-13 incicating the pump channel
-    #volume is the volume in µL
-    #speed is a variable 0-1 going from 0µl/min to 4000µL/min
+    def dispenseVolume(self, pump:int ,volume:int ,speed:int ,direction:int=1,read=False):
+        #pump is an index 0-13 incicating the pump channel
+        #volume is the volume in µL
+        #speed is a variable 0-1 going from 0µl/min to 4000µL/min
 
-    ser.write(bytes('{},PON,1234\r'.format(conf['pumpAddr'][pump]),'utf-8'))
-    ser.write(bytes('{},WFR,{},{}\r'.format(conf['pumpAddr'][pump],speed,direction),'utf-8'))
-    ser.write(bytes('{},WVO,{}\r'.format(conf['pumpAddr'][pump],volume),'utf-8'))
-    ser.write(bytes('{},WON,1\r'.format(conf['pumpAddr'][pump]),'utf-8'))
+        self.ser.write(bytes('{},PON,1234\r'.format(self.conf['pumpAddr'][pump]),'utf-8'))
+        self.ser.write(bytes('{},WFR,{},{}\r'.format(self.conf['pumpAddr'][pump],speed,direction),'utf-8'))
+        self.ser.write(bytes('{},WVO,{}\r'.format(self.conf['pumpAddr'][pump],volume),'utf-8'))
+        self.ser.write(bytes('{},WON,1\r'.format(self.conf['pumpAddr'][pump]),'utf-8'))
 
-    time_block = time.time()+volume/speed
-    _ = setBlock(pump,time_block)
+        time_block = time.time()+volume/speed
+        _ = setBlock(pump,time_block)
 
-    ans = ser.read(1000)
+        ans = self.ser.read(1000)
 
-    retc = return_class(
-        measurement_type="pump_command",
-        parameters={
-            "command": "dispenseVolume",
-            "parameters": {
-                "volume": volume,
-                "speed": speed,
-                "pump": pump,
-                "direction": direction,
-                "time_block": time_block,
+        retc = return_class(
+            measurement_type="pump_command",
+            parameters={
+                "command": "dispenseVolume",
+                "parameters": {
+                    "volume": volume,
+                    "speed": speed,
+                    "pump": pump,
+                    "direction": direction,
+                    "time_block": time_block,
+                },
             },
-        },
-        data={'serial_response':ans},
-    )
-    return retc
+            data={'serial_response':ans},
+        )
+        return retc
 
-@app.get("/pump/stopPump")
-def stopPump(pump:int):
-    #this stops a selected pump and returns the nessesary information the seed is recorded as zero and direction as -1
-    #the reason for that is that i want to indicate that we manually stopped the pump
-    ser.write(bytes('{},WON,0\r'.format(conf['pumpAddr'][pump]), 'utf-8'))
-    time_block = time.time()
-    _ = setBlock(pump,time_block)
-    retc = return_class(
-        measurement_type="pump_command",
-        parameters={
-            "command": "stopPump",
-            "parameters": {
-                "pump": pump,
-                'speed': 0,
-                'volume': 0,
-                'direction': -1,
-                "time_block": time_block,
+    def stopPump(self, pump:int):
+        #this stops a selected pump and returns the nessesary information the seed is recorded as zero and direction as -1
+        #the reason for that is that i want to indicate that we manually stopped the pump
+        self.ser.write(bytes('{},WON,0\r'.format(conf['pumpAddr'][pump]), 'utf-8'))
+        time_block = time.time()
+        _ = self.setBlock(pump,time_block)
+        retc = return_class(
+            measurement_type="pump_command",
+            parameters={
+                "command": "stopPump",
+                "parameters": {
+                    "pump": pump,
+                    'speed': 0,
+                    'volume': 0,
+                    'direction': -1,
+                    "time_block": time_block,
+                },
             },
-        },
-        data=None,
-    )
-    return retc
+            data=None,
+        )
+        return retc
 
-@app.on_event("shutdown")
-def shutdown():
-    for i in range(14):
-        stopPump(i)
-    ser.close()
-    retc = return_class(
-        measurement_type="pump_command",
-        parameters={"command": "shutdown"},
-        data={'data':'shutdown'}
-    )
-    return retc
-
-
-if __name__ == "__main__":
-    # the pump can only communicate over com port 1 at 9600 baud
-    conf = dict(port='COM1', baud=9600, timeout=1,
-                pumpAddr={i: i + 21 for i in range(14)},  # numbering is left to right top to bottom
-                pumpBlockings={i: time.time() for i in range(14)},  # init the blockings with now
-                )
-    #i set the serial connection here once
-    ser = serial.Serial(conf['port'], conf['baud'], timeout=conf['timeout'])
-
-    uvicorn.run(app, host="127.0.0.1", port=13370)
+    def shutdown():
+        for i in range(14):
+            stopPump(i)
+        ser.close()
+        retc = return_class(
+            measurement_type="pump_command",
+            parameters={"command": "shutdown"},
+            data={'data':'shutdown'}
+        )
+        return retc

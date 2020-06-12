3d2cb0a57d3b075d84f9c12a658be19d1d516a28
Fuzhan R <fuzhanrahmanian@gmail.com>
2020-06-12 10:05:34 +0200

    updated

--- driver/mecademic_server.py
+++ driver/mecademic_server.py
@@ -1,1 +1,24 @@
+import serial
+import time
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
 
+app = FastAPI(title="Pump server V1",
+    description="This is a very fancy pump server",
+    version="1.0",)
+
+class return_class(BaseModel):
+    measurement_type: str = None
+    parameters: dict = None
+    data: dict = None
+
+
+
+
+
+
+if __name__ == "__main__":
+
+    p = Mecademic()
+    uvicorn.run(app, host="127.0.0.1", port=13370)

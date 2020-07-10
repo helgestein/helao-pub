2ea4c13ba3d8f47d17d443f58998a15a12fd227f
Fuzhan R <fuzhanrahmanian@gmail.com>
2020-07-10 15:41:28 +0200

    i think kadi actually works now? still confused by a few things --jack-- also I didn't test any of the servers but I don't think we ever had any errors caused by them.

--- driver/kadi_driver.py
+++ driver/kadi_driver.py
@@ -3,7 +3,7 @@ from kadi_apy.lib.records import Record
 from kadi_apy.lib.collections import Collection
 import json
 import time
-import pandas as pd
+import pandas
 
 class kadi():
     def __init__(self,conf):
@@ -13,14 +13,15 @@ class kadi():
     def addRecord(self,ident,title,filed,visibility='private',meta=None): #filed is a json 
         #create a record
         #visibility must be 'public' or 'private'
+        #if not None, meta must be serialized dict
+        #I think that this probably works, but that there may still be some typing confusion. If filed is already a json, why do we need to upload it as json.dumps(filed)?
         record = Record(identifier=ident, title=title, visibility=visibility)
         record.upload_string_to_file(string=json.dumps(filed),file_name='{}_{}.json'.format(ident,time.time_ns()))
         #add metadatum#
-        print(meta)
-        print(type(meta))
-        df = pd.json_normalize(json.loads(meta), sep='_')
-        meta = df.to_dict(orient='records')[0]
-        record.add_metadatum(metadatum=meta, force=True)
+        if meta != None:
+            df = pandas.json_normalize(json.loads(meta), sep='_')
+            meta = df.to_dict(orient='records')[0]
+            record.add_metadatum(metadatum=meta, force=True)
 
     def addCollection(self,identifier,title,visibility):
         #create collection

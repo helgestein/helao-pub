f53a7cce278572c8b15c6eca8e4f10d0b2d2248c
Fuzhi_Surface <fuzhanrahmanian@gmail.com>
2020-07-15 14:57:32 +0200

    An example for the sequence of the an experiment is added

--- action/pumping.py
+++ action/pumping.py
@@ -11,29 +11,6 @@ class return_class(BaseModel):
     measurement_type: str = None
     parameters: dict = None
     data: dict = None
-    
-@app.get("/pumping/formulation_succ/")
-def formulation_successive(comprel: list, pumps: list, speed: int, totalvol: int):
-    #make sure the comprel makes sense
-    comprel = [i/sum(comprel) for i in comprel]
-    retl = []
-    for c,p in zip(comprel,pumps):
-        #someone check this logic ...
-        #we pump at different speeds for formulation in the droplet coming out
-        #to make all pumps stop at roughly the same time the volume per pump is also
-        #adjusted 
-        v = int(totalvol*c)
-        s = int(speed*c)
-        res = requests.get("{}/pump/dispenseVolume".format(pumpurl), 
-                            params={'pump':p,'volume':v,'speed':speed,
-                                    'direction':1,'read':False,'stage':False}).json()
-        retl.append(res)
-
-    retc = return_class(measurement_type='echem_measure',
-                        parameters= {'command':'measure',
-                                    'parameters':{'comprel':comprel,'pumps':pumps,'speed':speed,'totalvol':totalvol}},
-                        data = {'data':retl})
-    return retc
 
 @app.get("/pumping/formulation/")
 def formulation(comprel: list, pumps: list, speed: int, totalvol: int):
@@ -54,7 +31,7 @@ def formulation(comprel: list, pumps: list, speed: int, totalvol: int):
     retl.append(requests.get("{}/pump/allOn".format(pumpurl), 
                     params={'time':totalvol/speed}).json())
 
-    retc = return_class(measurement_type='echem_measure',
+    retc = return_class(measurement_type='pumping',
                         parameters= {'command':'measure',
                                     'parameters':{'comprel':comprel,'pumps':pumps,'speed':speed,'totalvol':totalvol}},
                         data = {'data':retl})
--- orchestrators/mischbares.py
+++ orchestrators/mischbares.py
@@ -1,3 +1,4 @@
+# In order to run the orchatrator which is at the highest level of Helao, all servers should be started. 
 import requests
 import time
 import sys 
@@ -8,9 +9,9 @@ from mischbares_small import config
 from fastapi import FastAPI
 from pydantic import BaseModel
 import json
-import requests
-
-# In order to run the orchatrator which is at the highest level of Helao, all servers should be started. 
+import uvicorn
+from typing import List
+from fastapi import FastAPI, Query
 
 #Action book 
 # still motor functions need to be added. 
@@ -21,6 +22,60 @@ sdc_std = ['movement/matrixRotation','movement/moveToHome','movement/jogging','m
            'echem/cellonoff', 'echem/retrieve', 'forceAction/read', 'data/addrecord', 'data/addcollection'] 
 
 
+
+#This is a highly complex experiment spec
+experiment_spec = dict(
+                       soe=['movement/moveToHome_0','movement/alignment_0','movement/mvToWaste_0','pumping/formulation_0',
+                            'movement/removeDrop_0','movement/moveToHome_1','movement/mvToSample_0','forceAction/read_0',
+                            'echem/measure_0','pump/formulation_1','movement/moveToHome_2','movement/mvToWaste_1',
+                            'pumping/formulation_2','movement/removeDrop_1','movement/moveToHome_4','movement/mvToSample_1',
+                            'echem/measure_1','pump/formulation_3','data/addrecord_0'], 
+  # in params , you should give the input values.
+                       params = dict(moveToHome_0 = None,
+                                     mvToWaste_0 = dict(position={'x':0.0,'y':0.0}),
+                                     formulation_0 = dict(formulation= [0.2,0.2,0.2,0.2,0.2],
+                                                     pumps= [0,1,2,3,4],
+                                                     speed= 1000,
+                                                     direction= -1,
+                                                     stage= True,
+                                                     totalVol= 2000),
+                                     removeDrop_0 = dict(y= (-20)),
+                                     mvToSample_0 = dict(x= 20, y= 10),
+                                     measure_0= dict(procedure= 'ca',
+                                                    setpoints= dict(applypotential = {'Setpoint value': -0.5},
+                                                                     recordsignal= {'Duration': 300}),
+                                                    plot= False,
+                                                    onoffafter= 'off'),
+                                     fomulation_1 = dict(formulation=[1],
+                                                     pumps=[5],
+                                                     speed=4000,
+                                                     direction=1,
+                                                     stage=True,
+                                                     totalVol=2000),
+                                    mvToWaste_1 = dict(position= {'x':0.0,'y':0.0}),
+                                    fomulation_2 = dict(formulation= [1],
+                                                     pumps= [5],
+                                                     speed= 4000,
+                                                     direction= -1,
+                                                     stage= True,
+                                                     totalVol= 2000), 
+                                    removeDrop_1 = dict(y = -20), 
+                                    mvToSample_1 = dict(x= 18, y= 8), 
+                                    measure_1= dict(procedure= 'ca',
+                                                    setpoints= dict(applypotential = {'Setpoint value': -0.5},
+                                                                     recordsignal= {'Duration': 300}),
+                                                    plot= False,
+                                                    onoffafter= 'off'),
+                                    fomulation_3 = dict(formulation=[1],
+                                                     pumps=[5],
+                                                     speed=4000,
+                                                     direction=1,
+                                                     stage=True,
+                                                     totalVol=2000),
+                                    addrecord_0= dict(ident= 1,title= 'electrodeposition', filed= 'cu-No3',
+                                                      visibility='private',meta=None))
+
+                                
 #this orchestrator manages a list of experiments and expects 
 #dispense a formulation
 pumpurl = "http://{}:{}".format("127.0.0.1", "13370")
@@ -29,46 +84,33 @@ res = requests.get("{}/pumping/formulation".format(pumpurl),
 
 
 experiment_list = []
-#this is a highly complex experiment spec
-experiment_spec = dict(
-                       soe=['movement/home','movement/waste','pumpingDispense_0','movement/drop',
-                            'movement/home','movement/sample','echem/measure','pump/aspirate',
-                            'movement/home','movement/waste','pumping/dispense_2','movement/drop',
-                            'movement/home','movement/sample','echem/measure_2','pump/aspirate_2',
-                            'analyze/maxcurr','plan/al','data/save'],
-  # in params , you should give the input values.
-                       params = dict('home':None,
-                                     'waste':dict(position={'x':0,'y':0}),
-                                     'dispense':dict(formulation=[0.2,0.2,0.2,0.2,0.2],
-                                                     pumps=[0,1,2,3,4],
-                                                     speed=1000,
-                                                     direction=1,
-                                                     stage=True,totalVol=2000])),
-                                     'dispense_2':dict(formulation=[1],
-                                                     pumps=[5],
-                                                     speed=4000,
-                                                     direction=1,
-                                                     stage=True,totalVol=2000])),
-                                     'drop':dict('delta_x':20),
-                                     'measure':dict('procedure'='ca',
-                                                    'setpoints'=dict('applypotential' = {'Setpoint value': -0.5},
-                                                                     'recordsignal' = {'Duration': 300}),
-                                                    'plot':False,
-                                                    'onoffafter':'off'),
-                                     'measure_2':dict('procedure'='ca',
-                                                      'setpoints'=dict('FHSetSetpointPotential': {'Setpoint value':0},
-                                                      'FHWait': {'Time':2},
-                                                      'CVLinearScanAdc164': {'StartValue': 0.0,
-                                                                            'UpperVertex': 1.5,
-                                                                            'LowerVertex': -0.25,
-                                                                            'NumberOfStopCrossings': 2,
-                                                                            'ScanRate': 0.02}),
-                                                    'plot':False,
-                                                    'onoffafter':'off'),
-                                     'aspirate':dict('amount'=1000),
-                                     'aspirate_2':dict('amount'=100),
-                                     'maxcurr':dict(pot=1.5,ref='OER'),
-                                     'al':dict(inputs=dict(dispense='formulation')))
+
+app = FastAPI()
+@app.get("/items/")
+def addToQueue(s: str):
+    d = json.load(s)
+    exp_list.append(d)
+    
+def execute(d)
+pop
+    for action_str in d['soe']:
+        action,fcnn = action_str.split('/')
+        request.get("/{}/{}".format(action,fcnn),
+                    params=d[fcnn.split['_'][0]])
+
+        
+if name == 'main':
+    exp_list = []
+    app 
+    
+    while True:
+        if len(exp_list)>0:
+            execute()
+
+
+
+
+
 
 while True:
     if experiment_list == []:

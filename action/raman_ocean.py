105e832ae0bf2913d3c16a8df8e10286ab9e1e66
helgestein <helge.stein@rub.de>
2020-07-17 11:10:30 +0200

    --jack-- ocean raman action, still untested. next i will debug.

---
+++ action/raman_ocean.py
@@ -0,0 +1,39 @@
+#implement the action-server for ocean optics raman
+import sys
+sys.path.append(r'../driver')
+sys.path.append(r'../config')
+sys.path.append(r'../server')
+from mischbares_small import config
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
+import json
+import requests
+
+app = FastAPI(title="ocean optics raman server V1", 
+    description="This is a fancy ocean optics raman spectrometer action server", 
+    version="1.0")
+
+
+class return_class(BaseModel):
+    measurement_type: str = None
+    parameters: dict = None
+    data: dict = None
+
+
+@app.get("/oceanAction/read")
+def getData():
+    wavelengthData = requests.get("{}/ocean/wavelengths".format(url)).json()
+    intensityData = requests.get("{}/ocean/intensities".format(url)).json()
+    data = dict(wavelengths=wavelengthData,intensities=intensityData)
+    retc = return_class(measurement_type='raman_measure', 
+                            parameters={'command':'read_data'}, 
+                            data={'data':data})
+    return retc
+
+
+
+if __name__ == "__main__":
+    url = "http://{}:{}".format(config['servers']['oceanServer']['host'],config['servers']['oceanServer']['port'])
+    uvicorn.run(app,host=config['servers']['smallRamanServer']['host'],port=config['servers']['smallRamanServer']['port'])
+    print("instantiated ocean optics raman action")

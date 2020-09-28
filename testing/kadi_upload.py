854a97098434a80e59cd8e2ecb9cd62a57eb958e
darthlegit <jkflowers@protonmail.com>
2020-09-28 13:01:58 +0200

    a few kadi updates

--- testing/kadi_upload.py
+++ testing/kadi_upload.py
@@ -2,13 +2,21 @@ import os
 import sys
 sys.path.append("../action")
 import kadi_action as ka
+from mischbares_small import config
 
 
 if __name__ == "__main__":
-    ka.url = "http://127.0.0.1:13376"
-    ka.addCollection("demo","uploading a bunch of initial echem data to see how it looks","public")
-    for filename in os.listdir(r"D:\kadiupload"):
+    soe = ["data/addCollection_0"]
+    params = dict("addCollection_0"=dict(identifier="",title=""))
+    #ka.addCollection("demo","uploading a bunch of initial echem data to see how it looks","private")
+    i=0
+    for filename in os.listdir(r"C:\"):
         ident = filename.split("_")[0]
-        if not ka.recordExists(ident):
-            ka.makeRecordFromFile(filename,r"D:\kadiupload",visibility="public")
-        ka.addRecordToCollection("demo",ident)
+        #if not ka.recordExists(ident):
+            #ka.makeRecordFromFile(filename,r"D:\kadiupload",visibility="private")
+        soe.append("data/makeRecordFromFile_{}".format(i))
+        params.update(dict("makeRecordFromFile_{}".format(i)=dict(filename=filename,filepath="")))
+        #ka.addRecordToCollection("demo",ident)
+        soe.append("data/addRecordToCollection_{}".format(i))
+        params.update(dict("addRecordToCollection_{}".format(i)=dict(identCollection="",identRecord=ident)))
+        i += 1

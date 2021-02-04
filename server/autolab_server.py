9d541e4e0f2f158612b6f3ccab2e6d23647d5b1b
fuzhanrahmanian <fuzhanrahmanian@gmail.com>
2021-02-04 16:33:22 +0100

    autolab_server is modifies and units are added.

--- server/autolab_server.py
+++ server/autolab_server.py
@@ -5,12 +5,13 @@ sys.path.append("../driver")
 from autolab_driver import Autolab
 from mischbares_small import config
 import uvicorn
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, WebSocket
 from pydantic import BaseModel
 import json
 from typing import List
 import os
 import time
+import re
 app = FastAPI(title="Autolab server V1",
     description="This is a very fancy autolab server",
     version="1.0",)
@@ -26,58 +27,47 @@ class Item(BaseModel):
 class ItemList(BaseModel):
     items: List[Item]
 
+@app.on_event("startup")
+def startup_event():
+    global a
+    a = Autolab(config['autolab'])
+
+
 @app.get("/potentiostat/ismeasuring")
 def ismeasuring():
     ret = a.ismeasuring()
-    retc = return_class(measurement_type='potentiostat_autolab',
-                        parameters= {'command':'ismeasuring',
-                                    'parameters':None},
-                        data = {'ismeasuring':ret})
+    retc = return_class(parameters= None,data = {'ismeasuring':ret})
     return retc
 
 @app.get("/potentiostat/potential")
 def potential():
     ret = a.potential()
-    retc = return_class(measurement_type='potentiostat_autolab',
-                        parameters= {'command':'getpotential',
-                                    'parameters':None},
-                        data = {'potential':ret})
+    retc = return_class(parameters= None,data = {'potential':ret,'units':'V'})
     return retc
 
 @app.get("/potentiostat/current")
 def current():
     ret = a.current()
-    retc = return_class(measurement_type='potentiostat_autolab',
-                        parameters= {'command':'getcurrent',
-                                    'parameters':None},
-                        data = {'current':ret})
+    retc = return_class(parameters= None,data = {'current':ret,'units':'A'})
     return retc
 
 @app.get("/potentiostat/setcurrentrange")
 def setCurrentRange(crange: str):
     a.setCurrentRange(crange)
-    retc = return_class(measurement_type='potentiostat_autolab',
-                        parameters= {'command':'setcurrentrange',
-                                    'parameters':crange},
-                        data = {'currentrange':crange})
+    res = [re.findall(r'(\d+)(\w+)', crange)[0]]
+    retc = return_class(parameters= {'parameters':crange,'units': res[0][1]},data = None)
     return retc
 
 @app.get("/potentiostat/setstability")
 def setStability(stability:str):
     a.setStability(stability)
-    retc = return_class(measurement_type='potentiostat_autolab',
-                        parameters= {'command':'setStability',
-                                    'parameters':stability},
-                        data = {'currentrange':stability})
+    retc = return_class(parameters={'stability':stability},data = None)
     return retc
 
 @app.get("/potentiostat/appliedpotential")
 def appliedPotential():
     ret = a.appliedPotential()
-    retc = return_class(measurement_type='potentiostat_autolab',
-                        parameters= {'command':'appliedpotential',
-                                    'parameters':None},
-                        data = {'appliedpotential':ret})
+    retc = return_class(parameters= None,data = {'appliedpotential':ret,'units':'V'})
     return retc
 
 @app.websocket("/ws")
@@ -85,59 +75,39 @@ async def websocket_messages(websocket: WebSocket):
     await websocket.accept()
     while True:
         data = await a.q.get()
-        data = {k: [v] for k, v in zip(["t_s", "Ewe_V", "Ach_V", "I_A"], data)}
-        await websocket.send_text(json.dumps(time.time.now()))
+        print('data: '+str(data))
+        data = {k: [v] for k, v in zip(["t_s", "freq", "Ewe_V", "Ach_V", "Z_real", "Z_imag", "phase", "modulus", "I_A"], data)}
+        await websocket.send_text(json.dumps(time.time()))
         await websocket.send_text(json.dumps(data))
+
 
 
 @app.get("/potentiostat/abort")
 def abort():
     ret = a.abort()
-    retc = return_class(measurement_type='potentiostat_autolab',
-                        parameters= {'command':'abort',
-                                    'parameters':None},
-                        data = {'abort':True})
+    retc = return_class(parameters= None,data= None)
     return retc
 
 @app.get("/potentiostat/cellonoff")
 def CellOnOff(onoff:str):
     a.CellOnOff(onoff)
-    retc = return_class(measurement_type='potentiostat_autolab',
-                        parameters= {'command':'cellonoff',
-                                    'parameters':onoff},
-                        data = {'onoff':onoff})
+    retc = return_class(parameters= {'onoff':onoff},data = None)
     return retc
-'''
+
 @app.get("/potentiostat/measure")
-def performMeasurement(procedure: str,setpoint_key:str ,setpoint_value:str ,plot:str,onoffafter:str,safepath:str,filename:str, parseinstructions:str):
-    print('{}, {}'.format(setpoint_key, setpoint_value))
-    setpoint_keys = list(eval(setpoint_key))
-    setpoint_values = list(eval(setpoint_value))
-    parseinstruction = [parseinstructions]
-    for q in [procedure,setpoint_keys,setpoint_values,plot,onoffafter,safepath,filename, parseinstruction]:
-        print(q)
-    a.performMeasurement(procedure,setpoint_keys,setpoint_values,plot,onoffafter,safepath,filename, parseinstruction)
-    retc = return_class(measurement_type='potentiostat_autolab',
-                    parameters= {'command':'measure',
-                                'parameters':dict(procedure=procedure,setpoint_keys=setpoint_keys,setpoint_values=setpoint_values,
-                                                  plot=plot,onoffafter=onoffafter,safepath=safepath,filename=filename, parseinstruction= parseinstruction)},
-                    data = {'data':None})
-    return retc
-'''
-@app.get("/potentiostat/measure")
-def performMeasurement(procedure: str,setpointjson: str ,plot:str,onoffafter:str,safepath:str,filename:str, parseinstructions:str):
+async def performMeasurement(procedure: str,setpointjson: str ,plot:str,onoffafter:str,safepath:str,filename:str, parseinstructions:str):
     setpoints = eval(setpointjson)
     #setpoint_keys = list(setpoints.keys())
     #setpoint_values = [setpoints[k] for k in setpoint_keys]
 
 
     parseinstruction = [parseinstructions]
-    a.performMeasurement(procedure,setpoints,plot,onoffafter,safepath,filename, parseinstruction)
+    data = await a.performMeasurement(procedure,setpoints,plot,onoffafter,safepath,filename, parseinstruction)
     retc = return_class(measurement_type='potentiostat_autolab',
                     parameters= {'command':'measure',
                                 'parameters':dict(procedure=procedure,setpointjson= setpointjson,
                                                   plot=plot,onoffafter=onoffafter,safepath=safepath,filename=filename, parseinstruction= parseinstruction)},
-                    data = {'data':None})
+                    data = {'data':data})
     return retc
 
 
@@ -147,20 +117,13 @@ def retrieve(safepath:str,filename:str):
     path = os.path.join(conf['safepath'],conf['filename'])
     with open(path.replace('.nox', '_data.json'), 'r') as f:
         ret = json.load(f)
-    retc = return_class(measurement_type='potentiostat_autolab',
-                    parameters= {'command':'retrieve',
-                                'parameters':conf},
-                    data = {'appliedpotential':ret})
+    retc = return_class(parameters= {'safepath':safepath,'filename':filename},
+                        data = {'appliedpotential':ret})
     return retc
 
 @app.on_event("shutdown")
 def disconnect():
     a.disconnect()
-    retc = return_class(measurement_type='potentiostat_autolab',
-                        parameters= {'command':'disconnect',
-                                    'parameters':None},
-                        data = {'data':None})
-    return retc
 
 if __name__ == "__main__":
     autolab_conf = config['autolab']

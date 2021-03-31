a89608d76567a06f38dff80f39724362ddd32926
jack f <jkflowers@protonmail.com>
2021-03-31 16:03:11 +0200

    first round of testing of launch script changes. does not work yet

--- server/owis_server.py
+++ server/owis_server.py
@@ -9,7 +9,7 @@ helao_root = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 sys.path.append(os.path.join(helao_root, 'config'))
 sys.path.append(os.path.join(helao_root, 'driver'))
 from owis_driver import owis
-config = import_module(sys.argv[1]).config
+config = import_module(sys.argv[1][:-3]).config
 
 
 app = FastAPI(title="owis driver", 

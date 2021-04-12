ea111959842c3c31fe3e8bb39880f70734b11be1
darthlegit <jkflowers@protonmail.com>
2021-04-12 13:58:37 +0200

    keeping up with the latest changes to kadi

--- driver/kadi_driver.py
+++ driver/kadi_driver.py
@@ -1,41 +1,37 @@
-from kadi_apy import KadiAPI
-from kadi_apy import Record
-from kadi_apy import Collection
+from kadi_apy import KadiManager
 import json
 import time
-import pandas
 import os
 
 class kadi():
     def __init__(self,conf):
-        KadiAPI.token = conf['PAT']
-        KadiAPI.host = conf['host']
+        self.manager = KadiManager(token=conf['PAT'],host=conf['host'])
 
     def addRecord(self,ident,title,visibility='private'):
         #create a record
         #visibility must be 'public' or 'private'
-        record = Record(identifier=ident,title=title,visibility=visibility,create=True)
+        record = self.manager.record(identifier=ident,title=title,visibility=visibility,create=True)
 
     def addCollection(self,ident,title,visibility='private'):
         #create collection
-        collection = Collection(identifier=ident,title=title,visibility=visibility,create=True)
+        collection = self.manager.collection(identifier=ident,title=title,visibility=visibility,create=True)
 
     def addRecordToCollection(self,identCollection,identRecord):
-        record = Record(identifier=identRecord)
-        record.add_collection_link(Collection(identifier=identCollection).id)
+        record = self.manager.record(identifier=identRecord)
+        record.add_collection_link(self.manager.collection(identifier=identCollection).id)
 
     def linkRecordToGroup(self,identGroup,identRecord):
-        record = Record(identifier=identRecord)
+        record = self.manager.record(identifier=identRecord)
         record.add_group_role(identGroup,"editor")
 
     def linkCollectionToGroup(self,identGroup,identCollection):
-        collection = Collection(identifier=identCollection)
+        collection = self.manager.collection(identifier=identCollection)
         collection.add_group_role(identGroup,"editor")
 
     def recordExists(self,ident):
         #determine whether a record with the given identifier exists
         try:
-            record = Record(identifier=ident)
+            record = self.manager.record(identifier=ident)
         except:
             return False
         return True
@@ -43,14 +39,14 @@ class kadi():
     def collectionExists(self,ident):
         #determine whether a collection with the given identifier exists
         try:
-            collection = Collection(identifier=ident)
+            collection = self.manager.collection(identifier=ident)
         except:
             return False
         return True
 
     def addFileToRecord(self,identRecord,filed):
         #if file is a filepath, upload from that path, if file is a json, upload directly
-        record = Record(identifier=identRecord)
+        record = self.manager.record(identifier=identRecord)
         try: 
             json.loads(filed)
             record.upload_string_to_file(string=filed,file_name='{}_{}.json'.format(identRecord,time.time_ns()))
@@ -58,12 +54,12 @@ class kadi():
             record.upload_file(file_path=filed)
 
     def addMetadataToRecord(self,identRecord,meta):
-        record = Record(identifier=identRecord)
+        record = self.manager.record(identifier=identRecord)
         record.add_metadata(json.loads(meta),True)
 
     def downloadFilesFromRecord(self,ident,filepath):
         #download all files from record
-        record = Record(identifier=ident)
+        record = self.manager.record(identifier=ident)
         response = record.get_filelist(per_page=100).json()
         for i in range(response['_pagination']['total_pages']):
             page = record.get_filelist(page=i,per_page=100).json()
@@ -72,7 +68,7 @@ class kadi():
 
     def downloadFilesFromCollection(self,ident,filepath):
         #download all files from all records in collection
-        collection = Collection(identifier=ident)
+        collection = self.manager.collection(identifier=ident)
         response = collection.get_records(per_page=100).json()
         for i in range(response['_pagination']['total_pages']):
             page = collection.get_records(page=i,per_page=100).json()
@@ -80,5 +76,5 @@ class kadi():
                 self.downloadFilesFromRecord(item['identifier'],filepath)
 
     def isFileInRecord(self,ident,filename):
-        record = Record(identifier=ident)
+        record = self.manager.record(identifier=ident)
         return record.has_file(filename)

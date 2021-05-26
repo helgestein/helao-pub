9b8f2e49668e556af3ea138aded6717d48ddfb3d
darthlegit <jkflowers@protonmail.com>
2021-05-26 11:16:10 +0200

    now will notice if a server closes randomly, or fails to properly open or close. next thing i want to add is the ability to call arbitrary actions from the interface

--- testing/helao_interface.py
+++ testing/helao_interface.py
@@ -23,8 +23,21 @@ window = gui.Window('HELAO',layout)
 servers = {}
 
 while True:
-    event, values = window.read()
-    if event.split('-')[0] in config['launch']['server'] or event.split('-')[0] in config['launch']['action'] or event.split('-')[0] in config['launch']['orchestrator']:
+    event, values = window.read(timeout=1000)
+    ps = [p.pid for p in list(psutil.process_iter())]
+    closed = []
+    for server in servers.items():
+        if not (server[1][1].pid in ps and server[1][0].pid in ps):
+            print(f"server {server[0]} seems to have closed unexpectedly")
+            closed.append(server[0])
+            window[server[0]+"-open"].update(disabled = False)
+            window[server[0]+"-close"].update(disabled = True)
+            window[server[0]+"-refresh"].update(disabled = True)
+    for c in closed:
+        del servers[c]
+    if event == gui.WIN_CLOSED:
+        break
+    elif event.split('-')[0] in config['launch']['server'] or event.split('-')[0] in config['launch']['action'] or event.split('-')[0] in config['launch']['orchestrator']:
         s = event.split('-')[0]
         api =  "server" if event.split('-')[0] in config['launch']['server'] else "action" if event.split('-')[0] in config['launch']['action'] else "orchestrators"
         if event.split('-')[1] == 'open':
@@ -40,11 +53,13 @@ while True:
                         p1 = p
                     if p.name() == 'python.exe':
                         p2 = p
-            assert p1.pid == p0.pid and p2 != None
-            servers.update({s:(p0,p2)})
-            window[event].update(disabled = True)
-            window[s+"-close"].update(disabled = False)
-            window[s+"-refresh"].update(disabled = False)
+            if not (p1.pid == p0.pid and p2 != None):
+                print(f"server {s} failed to start")
+            else:
+                servers.update({s:(p0,p2)})
+                window[event].update(disabled = True)
+                window[s+"-close"].update(disabled = False)
+                window[s+"-refresh"].update(disabled = False)
         elif event.split('-')[1] == 'close':
             print(f"Killing {api}/{s}.py")
             l = list(psutil.process_iter())
@@ -59,11 +74,13 @@ while True:
                         terminated[0] = servers[s][0].pid
                     if p.name() == 'python.exe':
                         terminated[1] = servers[s][1].pid
-            assert terminated == [servers[s][0].pid,servers[s][1].pid]
-            del servers[s]
-            window[s+"-open"].update(disabled = False)
-            window[event].update(disabled = True)
-            window[s+"-refresh"].update(disabled = True)
+            if not terminated == [servers[s][0].pid,servers[s][1].pid]:
+                print(f"server {s} failed to close")
+            else: 
+                del servers[s]
+                window[s+"-open"].update(disabled = False)
+                window[event].update(disabled = True)
+                window[s+"-refresh"].update(disabled = True)
         elif event.split('-')[1] == 'refresh':
             print(f"Killing {api}/{s}.py")
             l = list(psutil.process_iter())
@@ -78,27 +95,29 @@ while True:
                         terminated[0] = servers[s][0].pid
                     if p.name() == 'python.exe':
                         terminated[1] = servers[s][1].pid
-            assert terminated == [servers[s][0].pid,servers[s][1].pid]
-            del servers[s]
-            window[s+"-open"].update(disabled = False)
-            window[event].update(disabled = True)
-            window[s+"-refresh"].update(disabled = True)
+            if not terminated == [servers[s][0].pid,servers[s][1].pid]:
+                print(f"server {s} failed to close")
+            else: 
+                del servers[s]
+                window[s+"-open"].update(disabled = False)
+                window[event].update(disabled = True)
+                window[s+"-refresh"].update(disabled = True)
 
-            cmd = ["python", f"{api}/{s}.py", sys.argv[1]]
-            print(f"Starting {api}/{s}")
-            p0 = subprocess.Popen(cmd, cwd=helao_root, shell=True)
-            time.sleep(.1)
-            p1,p2 = None, None
-            for p in psutil.process_iter():
-                if p not in l2:
-                    if p.name() == 'cmd.exe':
-                        p1 = p
-                    if p.name() == 'python.exe':
-                        p2 = p
-            assert p1.pid == p0.pid
-            servers.update({s:(p0,p2)})
-            window[event].update(disabled = True)
-            window[s+"-close"].update(disabled = False)
-            window[s+"-refresh"].update(disabled = False)
-    elif event == gui.WIN_CLOSED:
-        break
+                cmd = ["python", f"{api}/{s}.py", sys.argv[1]]
+                print(f"Starting {api}/{s}")
+                p0 = subprocess.Popen(cmd, cwd=helao_root, shell=True)
+                time.sleep(.1)
+                p1,p2 = None, None
+                for p in psutil.process_iter():
+                    if p not in l2:
+                        if p.name() == 'cmd.exe':
+                            p1 = p
+                        if p.name() == 'python.exe':
+                            p2 = p
+                if not (p1.pid == p0.pid and p2 != None):
+                    print(f"server {s} failed to start")
+                else:
+                    servers.update({s:(p0,p2)})
+                    window[event].update(disabled = True)
+                    window[s+"-close"].update(disabled = False)
+                    window[s+"-refresh"].update(disabled = False)

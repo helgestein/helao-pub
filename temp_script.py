5dc6c003c040e3bf3e07414aa95ef3249aec3066
Alexey Sanin <saninalexius@gmail.com>
2024-11-14 13:02:08 +0100

    abundant files are removed

--- temp_script.py
+++
@@ -1,8 +0,0 @@
-import requests 
-from config.sdc_cyan import config 
-def test_fnc(action, params): 
-   server = 'dobot' 
-   action = action 
-   params = params 
-   r = requests.get('http://{}:{}/{}/{}'.format(config['servers']['dobot']['host'], config['servers']['dobot']['port'], server, action), params= params).json() 
-   print(r) 

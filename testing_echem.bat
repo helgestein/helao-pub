437561abb803177956b93a48d9503ad048474988
Alexey Sanin <76043967+saninalexey@users.noreply.github.com>
2024-12-18 16:11:34 +0100

    Delete testing_echem.bat

--- testing_echem.bat
+++
@@ -1,9 +0,0 @@
-@ECHO on
-call C:\ProgramData\anaconda3\condabin\conda.bat activate helao
-cd C:\Users\juliu\helao-dev
-
-title testing_echem
-ipython testing_palmsens_script.py
-ipython -i
-
-PAUSE

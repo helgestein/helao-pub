c77886462a86ff4b38f810735f43b9d96916d31e
Alexey Sanin <saninalexius@gmail.com>
2025-02-04 20:23:09 +0100

    not used files are deprecated

--- config/palmsens_config.py
+++
@@ -1,8 +0,0 @@
-#config = {}
-config = dict()
-
-config["PalmsensSDK_python"] = r"C:\Users\juliu\Documents\PSPythonSDK"
-
-config["savepath_raw"] = r"C:\Users\juliu\helao-dev\temp\palmsens_data"
-
-config["log_folder"] = r"C:\Users\juliu\helao-dev\temp\palmsens_data\log"
